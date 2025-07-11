1af3361aaf20c3fb36a377b5e2cba6e5fd0d6021
Xitr-oss <ossxitr@gmail.com>
2025-07-11 08:06:27 +0530

    six

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,33 +1,14 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-
-//     mongoose.connection.on('connected',() => {
-//         console.log("DB Connected");
-//     })
-
-//     await mongoose.connect(`${process.env.MONGODB_URI}e-commerce`)
-
-// }
-
-// export default connectDB;
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
-  mongoose.connection.on('connected', () => {
-    console.log("DB Connected");
-  });
 
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'e-commerce',          // Specify your DB here
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (error) {
-    console.error("DB connection error:", error);
-  }
-};
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}e-commerce`)
+
+}
 
 export default connectDB;
+
